aa287dd096d006303b42a49b925cb55a12541584
Jordan Sinou <sinoujordan@gmail.com>
2022-05-24 13:57:43 +0200

    add ref article/user

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const authorController = require('../controllers/authorController');
 var router = express.Router();
 
 const userController = require('../controllers/userController')
@@ -6,5 +7,11 @@ const userController = require('../controllers/userController')
 /* GET home page. */
 router.route('/').get(userController.getUsers)
 router.route('/user').post(userController.createUser)
+router.route('/user/:id').delete(userController.deleteUser)
+router.route('/user/:id').get(userController.getUser)
+
+router.route('/author').post(authorController.createAuthor)
+router.route('/article').post(authorController.createArticle)
+router.route('/article/:author').get(authorController.findArticlesByUserId)
 
 module.exports = router;
